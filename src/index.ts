269723a84227edd923c1bbefa88bdafb4adea55c
xuxiuquan@kaochong.com <xuxiuquan@kaochong.com>
2020-01-07 23:44:27 +0800

    add: max thread count

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import { ThreadManager, ThreadManagerConfig} from './thread/thread-manager';
 
 class BetterWorker {
+  static MaxThread: number = (navigator.hardwareConcurrency || 4) + 2;
   private option: any;
   private tm: ThreadManager;
   constructor(workerPaths: Array<string>) {
+    if (workerPaths.length > BetterWorker.MaxThread) {
+      throw new Error(`init fail, max thread count is:${BetterWorker.MaxThread}`);
+    }
     this.tm = new ThreadManager(workerPaths);
   }
 
